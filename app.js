184cfb4b4159fdb3f378bdff4a81031ad040e95b
Lucas Camargo <lucascam_8@hotmail.com>
2021-11-25 22:19:49 -0300

    chat

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const {Server} = require('socket.io')
+const app = express()
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT,()=>{
+    console.log("listening to port "+PORT)
+})
+
+app.use(express.static(__dirname+'/public'))
+const io = new Server(server)
+let messages = []
+
+
+io.on('connection',socket=>{
+    console.log("cliente conectado")
+    socket.emit('welcome', 'BIENVENIDO A MI SOCKET BIEN SOCKETSOSO')
+    socket.on('message',data=>{
+        messages.push(data)
+        io.emit('messagelog',messages)
+    })
+})
+
